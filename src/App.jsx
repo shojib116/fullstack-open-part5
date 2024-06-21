e5809652b2ce5001a8a279ad2b07827c1ed89011
Nazmul Hasan Shajib <shojib116@gmail.com>
2024-06-21 16:08:15 +0600

    5.1-completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  if (user === null) {
+    return (
+      <div>
+        <LoginForm setUser={setUser} />
+      </div>
+    );
+  }
 
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import loginServices from "../services/login";
+
+const LoginForm = ({ setUser }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginServices.login({ username, password });
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <h2>log in to application</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">username</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
+        />
+        <br />
+        <label htmlFor="password">password</label>
+        <input
+          type="text"
+          name="password"
+          id="password"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+        <br />
+        <input type="submit" value="login" />
+      </form>
+    </>
+  );
+};
+
+export default LoginForm;
