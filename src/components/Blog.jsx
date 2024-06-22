2934cd793be38b0c62491d37671fed21c840abf5
Nazmul Hasan Shajib <shojib116@gmail.com>
2024-06-22 21:56:56 +0600

    5.7-completed

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} - {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} - {blog.author}{" "}
+        <input
+          type="button"
+          value={showDetails ? "hide" : "view"}
+          onClick={() => setShowDetails(!showDetails)}
+        />
+      </div>
+      {showDetails && (
+        <div>
+          {blog.url}
+          <br />
+          likes {blog.likes} <input type="button" value="like" />
+          <br />
+          {blog.user.name}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
